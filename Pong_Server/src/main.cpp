1bd6f7cbd96aeea4a32c5d3621ff6fb5e78a9276
AIE\s210991 <s210991@students.aie.edu.au>
2024-05-21 11:11:11 +0930

    Replaced custom default max player check with inbuilt one from API.

--- Pong_Server/src/main.cpp
+++ Pong_Server/src/main.cpp
@@ -4,18 +4,11 @@
 #include <BCNet/BCNetPacket.h>
 #include <BCNet/BCNetUtil.h>
 
-#define MAX_PLAYERS 2
-
 static BCNet::IBCNetServer *g_server;
 
 void OnConnected(const BCNet::ClientInfo &clientInfo)
 {
-	if (g_server->GetConnectedCount() > MAX_PLAYERS) // Ensure max players.
-	{
-		// TODO: Tell client that the server is full.
-		std::cout << "Kicked Client " << clientInfo.nickName << ": Server is FULL" << std::endl;
-		g_server->KickClient(clientInfo.id);
-	}
+
 }
 
 void PacketReceived(const BCNet::ClientInfo &clientInfo, const BCNet::Packet packet)
@@ -37,6 +30,8 @@ int main()
 
 	g_server->SetConnectedCallback(OnConnected);
 	g_server->SetPacketReceivedCallback(PacketReceived);
+
+	g_server->SetMaxClients(2);
 
 	g_server->Start();
 	g_server->Stop();
